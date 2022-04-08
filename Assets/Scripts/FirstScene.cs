273ce73f4db5dbc04cb86b113b510afcafe5d1dc
Morteza Nedaei <nedaei1375@gmail.com>
2022-04-08 21:44:59 +0000

    Changes of latest version

---
+++ Assets/Scripts/FirstScene.cs
@@ -0,0 +1,21 @@
+﻿using TapsellPlusSDK;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstScene : MonoBehaviour
+{
+    private const string TapsellPlusKey = "alsoatsrtrotpqacegkehkaiieckldhrgsbspqtgqnbrrfccrtbdomgjtahflchkqtqosa";
+
+    private void Start()
+    {
+        TapsellPlus.Initialize(TapsellPlusKey,
+            adNetworkName => Debug.Log(adNetworkName + " Initialized Successfully."),
+            error => Debug.Log(error.ToString()));
+        TapsellPlus.SetGdprConsent(true);
+    }
+
+    public void ChangeScenes(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/TapsellPlusSDK/models/TapsellPlusErrorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TapsellPlusSDK.models
+{
+    [Serializable]
+    public class TapsellPlusErrorModel : TapsellPlusAdModel
+    {
+        public string errorMessage;
+
+        public TapsellPlusErrorModel(string responseId, string zoneId, string errorMessage) : base(responseId, zoneId)
+        {
+            this.errorMessage = errorMessage;
+        }
+    }
+}
